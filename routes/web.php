e70c64cf4ee1468e23e853fb5b3f1734c8c24917
Debby95 <debbyjacksonstar@hotmail.it>
2022-08-11 15:22:15 +0200

    controller

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -20,3 +21,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/admin', 'Admin\HomeController@index')->name('admin.index');
